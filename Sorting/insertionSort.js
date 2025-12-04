d170e9c6b0719f15a28acf31e14f34377e5b0f93
Okamissu <okamissu@gmail.com>
2025-12-04 09:31:36 +0100

    feat: implement insertion sort algorithm

---
+++ Sorting/insertionSort.js
@@ -0,0 +1,34 @@
+/**
+ * Insertion Sort
+ *
+ * @function insertionSort
+ * @param {Array} array - The array of numbers to sort
+ * @returns {Array} The array sorted in ascending order (in-place)
+ *
+ * @description
+ * More detailed explanation if needed
+ *
+ * @pseudocode
+ * 1. Start by picking the 2nd element in the array
+ * 2. Compare it to the 1 before it and swap if needed
+ * 3. Continue to the next element - if in incorrect order, iterate through the sorted part to find correct place.
+ * 4. Repeat until sorted
+ *
+ * @complexity
+ * Time Complexity: ...
+ * Space Complexity: ...
+ */
+
+function insertionSort(array) {
+  for (let i = 1; i < array.length; i++) {
+    let currectVal = array[i]
+    let j;
+    for (j = i - 1; (j >= 0) & (array[j] > currectVal); j--) {
+      array[j + 1] = array[j]
+    }
+    array[j + 1] = currectVal
+  }
+  return array
+}
+
+console.log(insertionSort([2, 1, 9, 76, 4]))
