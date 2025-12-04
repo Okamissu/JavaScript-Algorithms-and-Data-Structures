883dcddaf0afeed7081d67bd7f5c4eda7aa04e36
Okamissu <okamissu@gmail.com>
2025-12-04 09:36:08 +0100

    chore: clean up insertion sort documentation and fix complexity

--- Sorting/insertionSort.js
+++ Sorting/insertionSort.js
@@ -5,9 +5,6 @@
  * @param {Array} array - The array of numbers to sort
  * @returns {Array} The array sorted in ascending order (in-place)
  *
- * @description
- * More detailed explanation if needed
- *
  * @pseudocode
  * 1. Start by picking the 2nd element in the array
  * 2. Compare it to the 1 before it and swap if needed
@@ -15,14 +12,14 @@
  * 4. Repeat until sorted
  *
  * @complexity
- * Time Complexity: ...
- * Space Complexity: ...
+ * Time Complexity: O(n^2)
+ * Space Complexity: O(1)
  */
 
 function insertionSort(array) {
   for (let i = 1; i < array.length; i++) {
     let currectVal = array[i]
-    let j;
+    let j
     for (j = i - 1; (j >= 0) & (array[j] > currectVal); j--) {
       array[j + 1] = array[j]
     }
