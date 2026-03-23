e6871aed2ef1b5f9304708f4447056968b540769
Okamissu <okamissu@gmail.com>
2026-03-23 09:58:31 +0100

    feat: implement pivot helper for quick sort

---
+++ Sorting/quickSort.js
@@ -0,0 +1,20 @@
+function pivot(arr, start = 0, end = arr.length - 1) {
+  let pivot = arr[start]
+  let swapIdx = start
+
+  for (let i = start + 1; i < arr.length; i++) {
+    if (pivot > arr[i]) {
+      swapIdx++
+      swap(arr, swapIdx, i)
+    }
+  }
+
+  swap(arr, start, swapIdx)
+  return swapIdx
+}
+
+const swap = (arr, i, j) => {
+  ;[arr[i], arr[j]] = [arr[j], arr[i]]
+}
+
+console.log(pivot([4, 8, 2, 1, 5, 7, 6, 3]))
