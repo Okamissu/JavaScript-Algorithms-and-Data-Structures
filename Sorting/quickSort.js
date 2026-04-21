3e97f2f7ccbefead232ef62351ab82d39041d78e
Okamissu <okamissu@gmail.com>
2026-04-21 08:48:48 +0200

    refactor: improve variable naming and loop condition in pivot function

--- Sorting/quickSort.js
+++ Sorting/quickSort.js
@@ -3,27 +3,25 @@ const swap = (arr, i, j) => {
 }
 
 function pivot(arr, start = 0, end = arr.length - 1) {
-  let pivot = arr[start]
-  let swapIdx = start
+  let pivotVal = arr[start]
+  let swapIndex = start
 
-  for (let i = start + 1; i < arr.length; i++) {
-    if (pivot > arr[i]) {
-      swapIdx++
-      swap(arr, swapIdx, i)
+  for (let i = start + 1; i <= end; i++) {
+    if (pivotVal > arr[i]) {
+      swapIndex++
+      swap(arr, swapIndex, i)
     }
   }
 
-  swap(arr, start, swapIdx)
-  return swapIdx
+  swap(arr, start, swapIndex)
+  return swapIndex
 }
 
 function quickSort(arr, left = 0, right = arr.length - 1) {
   if (left < right) {
     let pivotIdx = pivot(arr, left, right)
 
-    //left
     quickSort(arr, left, pivotIdx - 1)
-    //right
     quickSort(arr, pivotIdx + 1, right)
   }
   return arr
