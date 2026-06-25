fa3e45e6fe7eeee7fe5a206a8fd782af389f81e5
Okamissu <okamissu@gmail.com>
2026-06-25 08:51:04 +0200

    feat: implement Stack class with push and pop methods

---
+++ Stack/stack.js
@@ -0,0 +1,58 @@
+class Node {
+  constructor(value) {
+    this.value = value
+    this.next = null
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null
+    this.last = null
+    this.size = 0
+  }
+
+  push(value) {
+    const newNode = new Node(value)
+
+    if (!this.first) {
+      this.first = this.last = newNode
+    } else {
+      newNode.next = this.first
+      this.first = newNode
+    }
+
+    return ++this.size
+  }
+
+  pop() {
+    if (!this.first) return null
+
+    const popNode = this.first
+    this.first = popNode.next
+    this.size--
+
+    if (!this.first) {
+      this.last = null
+    }
+
+    popNode.next = null
+    return popNode
+  }
+}
+
+const stack = new Stack()
+
+stack.push(1)
+stack.push(2)
+stack.push(3)
+
+stack.pop()
+
+console.log(stack)
+stack.pop()
+
+console.log(stack)
+stack.pop()
+
+console.log(stack)
